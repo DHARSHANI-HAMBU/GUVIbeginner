f51b9828b155f553536dca51949b9829cff570d8
DHARSHANI-HAMBU <52707746+DHARSHANI-HAMBU@users.noreply.github.com>
2019-08-13 14:35:18 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,9 +1,15 @@
 #d
-n=input()
-a=0
-for i in range(0,len(n)):
-	a+=n[i]**3
-if a==n:
+def arm(n1):
+	d=0
+	m=0
+	while n1>0:
+		d=n1%10
+		m+=d*d*d
+		n1=n1//10
+	return m
+n=int(input())
+c=arm(n)
+if c==n:
 	print("yes")
 else:
 	print("no")
